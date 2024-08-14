48208fca9fdf3ca8610a18ec5c19dece3a562f10
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-08-14 14:49:22 +0200

    mysql does not like text primary column

--- packages/cli/src/databases/migrations/common/1723627610222-CreateInvalidAuthTokenTable.ts
+++ packages/cli/src/databases/migrations/common/1723627610222-CreateInvalidAuthTokenTable.ts
@@ -5,7 +5,7 @@ const tableName = 'invalid_auth_token';
 export class CreateInvalidAuthTokenTable1723627610222 implements ReversibleMigration {
 	async up({ schemaBuilder: { createTable, column } }: MigrationContext) {
 		await createTable(tableName)
-			.withColumns(column('token').text.primary, column('expiresAt').timestamp().notNull)
+			.withColumns(column('token').varchar(512).primary, column('expiresAt').timestamp().notNull)
 			.withIndexOn('token', true);
 	}
 
